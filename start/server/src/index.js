144620172fd39d017b094b5d28d00efcaf89e09d
tim0git <tim.doolan88@icloud.com>
2020-12-30 09:31:28 +0000

    push

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,1 +1,28 @@
-require('dotenv').config();
+require("dotenv").config();
+
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const { createStore } = require("./utils");
+const LaunchAPI = require("./datasources/launch");
+const UserAPI = require("./datasources/user");
+
+const resolvers = require("./resolvers");
+
+const store = createStore();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store }),
+  }),
+});
+
+server.listen().then(() => {
+  console.log(`
+      Server is running!
+      Listening on port 4000
+      Explore at https://studio.apollographql.com/dev
+    `);
+});
